0d7710cd48de03e8608c36519d4ceb255ae851ef
taal-12 <taofiquekhan535@gmail.com>
2025-10-02 07:26:19 +0530

    Simplify setup.py to fix package discovery error

--- setup.py
+++ setup.py
@@ -3,17 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name='movie-recommender-system',
     version='0.0.1',
-    author='TAOFIQUE KHAN',
-    author_email='taofiquekhan535@gmail.com',
-    description='A small example package for movie recommendation',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    python_requires='>=3.7',
-    install_requires=[
-        'streamlit',
-        # Add other specific dependencies from requirements.txt here
-        # but keep '-e .' in requirements.txt
-    ]
+    packages=find_packages(),
 )
