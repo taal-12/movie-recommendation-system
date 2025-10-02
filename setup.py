097a66ba0052f7d3157426daf57375de101a9ecb
taal-12 <taofiquekhan535@gmail.com>
2025-10-02 07:14:55 +0530

    Fix setup.py for Streamlit deployment

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-with open("README.md","r",encoding="utf-8") as fh:
-    long_description = fh.read()
-    
-AUTHOR_NAME = 'TAOFIQUE KHAN'
-SRC_REPO = 'src'
-LIST_OF_REQUIREMENTS = ['streamlit']
-
-setup (
-    name = SRC_REPO,
-    version = '0.0.1',
-    author = AUTHOR_NAME,
-    email = 'taofiquekhan535@gmail.com',
-    desciption = 'a small example package for movie recommendation',
-    long_description = long_description,
-    long_description_content_type = 'text/markdown',
-    package = [SRC_REPO] ,
-    python_requires = '>=3.7' ,
-    install_requires =LIST_OF_REQUIREMENTS ,
+setup(
+    name='movie-recommender-system',
+    version='0.0.1',
+    author='TAOFIQUE KHAN',
+    author_email='taofiquekhan535@gmail.com',
+    description='A small example package for movie recommendation',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    python_requires='>=3.7',
+    install_requires=[
+        'streamlit',
+        # Add other specific dependencies from requirements.txt here
+        # but keep '-e .' in requirements.txt
+    ]
 )
